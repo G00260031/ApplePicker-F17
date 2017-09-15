0914243b8e47951d20a93c70097ec5555223d75a
G00260031 <G00260031@stu.raritanval.edu>
2017-09-15 16:52:24 -0400

    Started Apple PG.431

---
+++ Assets/Scripts/AppleTree.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour {
+
+	public GameObject appleprefab;
+	public float speed = 1f;
+	public float leftAndRightEdge = 10f;
+	public float chanceToChangeDirections = .1f;
+	public float secondsBetweenAppleDrops = 1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
